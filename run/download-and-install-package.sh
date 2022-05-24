2947494e81fb8900327e756b7dd16d2d12c0a327
Frankie Gallina-Jones <frankieg@vmware.com>
2022-05-24 17:54:08 -0400

    use dpkg-deb -f to extract run image package info
    Signed-off-by: Rob Dimsdale-Zucker <rdimsdale@vmware.com>

--- run/download-and-install-package.sh
+++ run/download-and-install-package.sh
@@ -19,27 +19,15 @@ while read PACKAGE; do
       echo "Unsupported data format"
       exit 1
     fi
+
     source_package="$(dpkg-deb --showformat='${source:Package}' -W "$PACKAGE"*.deb)"
     source_version="$(dpkg-deb --showformat='${source:Version}' -W "$PACKAGE"*.deb)"
     source_upstream_version="$(dpkg-deb --showformat='${source:Upstream-Version}' -W "$PACKAGE"*.deb)"
-    if [[ "$(ls control.*)" == "control.tar.xz" ]]; then
-        unxz < control.tar.xz | tar x ./control && mv ./control "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Package: ${source_package}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Version: ${source_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Upstream-Version: ${source_upstream_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-    elif [[ "$(ls control.*)" == "control.tar.gz" ]]; then
-        tar xzf control.tar.gz ./control && mv ./control "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Package: ${source_package}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Version: ${source_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Upstream-Version: ${source_upstream_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-    elif [[ "$(ls control.*)" == "control.tar.zst" ]]; then
-        tar xzf control.tar.zst ./control && mv ./control "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Package: ${source_package}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Version: ${source_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-        echo "Source-Upstream-Version: ${source_upstream_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
-    else
-      echo "Unsupported control format"
-        exit 1
-    fi
-    rm debian-binary data.tar.* control.tar.*
+
+    dpkg-deb -f $PACKAGE*.deb > /tiny/var/lib/dpkg/status.d/$PACKAGE
+    echo "Source-Package: ${source_package}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
+    echo "Source-Version: ${source_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
+    echo "Source-Upstream-Version: ${source_upstream_version}" >> "/tiny/var/lib/dpkg/status.d/$PACKAGE"
+
+    rm -rf debian-binary data.tar.* control.tar.*
 done < packagelist
